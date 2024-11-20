62dbe6a9f0a910f93faf21ebe7f92db1e35579b1
ELCAZADOR5991 <123038857+ELCAZADOR5991@users.noreply.github.com>
2024-11-19 21:59:02 -0500

    modelo
    asf

--- src/App.js
+++ src/App.js
@@ -6,15 +6,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <span>Informe de Datos</span>
+        <span>Informe de Datos 25/8/366 Dias al Año</span>
         <div className="superior">
 
         </div>
                 
-        
-      </header>
+        <div className="contenido">
+          <h2>Contenido</h2>
 
-     
+        </div>
+      
+
+      <footer>
+        <h4>
+          Desarrollador Por Ing. Salcedo Manrique Christopher  <br></br>
+          Celar: 920783350
+        </h4>
+
+
+      </footer>
+      </header>
 
     </div>
   );
